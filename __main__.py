837f67251ede7c4b61813f595aef39fc26b2a54f
Steven Bue <skeeve11@gmail.com>
2022-03-19 09:53:56 -0500

    Actor movement set up KeyboardService set up

---
+++ __main__.py
@@ -0,0 +1,9 @@
+import pyray as pr
+from services.keyboardservice import KeyboardService
+from services.videoservice import VideoService
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
--- directing/director.py
+++ directing/director.py
@@ -1,4 +1,6 @@
 
 
 class Director:
-    pass
+    def __init__(self, keyboardservice, videoservice):
+        pass
+
